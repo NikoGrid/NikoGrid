38a956c6cf0feb09a1d053f7d2b98dfd1308ab6c
João Capucho <capucho@jcapucho.com>
2025-05-05 01:38:28 +0100

    Add sonarqube integration

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = {nixpkgs, ...}: let
+    supportedSystems = ["x86_64-linux"];
+
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+    nixpkgsFor = forAllSystems (system:
+      import nixpkgs {
+        inherit system;
+      });
+  in {
+    devShells = forAllSystems (system: let
+      pkgs = nixpkgsFor.${system};
+    in {
+      default = pkgs.mkShell {
+        packages = [
+          pkgs.maven
+        ];
+      };
+    });
+  };
+}
