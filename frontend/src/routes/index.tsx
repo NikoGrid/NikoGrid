5273783f2084bfc4ce8d222d23796f1bb0e3fb5a
Blosuhm <102238225+Blosuhm@users.noreply.github.com>
2025-05-31 22:24:56 +0100

    fix: Fixed nearby stations tests

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,8 +1,10 @@
 import StationDetails from "@/components/station-details";
 import Stations from "@/components/stations";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useCurrentPosition } from "@/hooks/use-current-position";
 import { createFileRoute } from "@tanstack/react-router";
-import { useState } from "react";
+import { useRef, useState, type ComponentRef } from "react";
 import { MapContainer, TileLayer } from "react-leaflet";
 
 export const Route = createFileRoute("/")({
@@ -12,12 +14,27 @@ export const Route = createFileRoute("/")({
 function RouteComponent() {
   const { location, locationAvailable } = useCurrentPosition();
   const [stationId, setStationId] = useState<number | null>(null);
+  const [lat, setLat] = useState("");
+  const [lon, setLon] = useState("");
+
+  const mapRef = useRef<ComponentRef<typeof MapContainer>>(null);
+
+  const updatePos = () => {
+    const coords = { lat: parseFloat(lat), lng: parseFloat(lon) } as const;
+    console.log(coords);
+
+    if (-90 > coords.lat || coords.lat > 90) return;
+    if (-180 > coords.lng || coords.lng > 180) return;
+
+    mapRef.current?.setView(coords, 9);
+  };
 
   if (location === null && locationAvailable) return null;
 
   return (
-    <main className="flex grow flex-col">
+    <main className="relative flex grow flex-col">
       <MapContainer
+        ref={mapRef}
         center={[
           location?.coords.latitude ?? 0,
           location?.coords.longitude ?? 0,
@@ -33,6 +50,23 @@ function RouteComponent() {
         <Stations setId={setStationId} />
       </MapContainer>
       <StationDetails stationId={stationId} setStationId={setStationId} />
+      <section className="absolute top-4 right-4 flex flex-col gap-2 md:flex-row">
+        <Input
+          data-test-id="lat-input"
+          className="w-32 bg-white"
+          placeholder="Latitude"
+          onChange={(e) => setLat(e.target.value)}
+        />
+        <Input
+          data-test-id="lon-input"
+          className="w-32 bg-white"
+          placeholder="Longitude"
+          onChange={(e) => setLon(e.target.value)}
+        />
+        <Button data-test-id="coords-button" type="button" onClick={updatePos}>
+          Go to
+        </Button>
+      </section>
     </main>
   );
 }
