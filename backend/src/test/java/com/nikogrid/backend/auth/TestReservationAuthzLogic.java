4bc650a1efa5d79cb73308259a37132dda0f3baa
Blosuhm <102238225+Blosuhm@users.noreply.github.com>
2025-06-08 21:11:31 +0100

    Applied review suggestions

--- backend/src/test/java/com/nikogrid/backend/auth/TestReservationAuthzLogic.java
+++ backend/src/test/java/com/nikogrid/backend/auth/TestReservationAuthzLogic.java
@@ -17,7 +17,7 @@ import java.util.UUID;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
-public class TestReservationAuthzLogic {
+class TestReservationAuthzLogic {
 
     @Mock
     private ReservationRepository reservationRepository;
